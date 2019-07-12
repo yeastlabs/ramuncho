694e453b4ffa982ccc1e17d075388d9d1306e88c
yeastco <35991686+yeastco@users.noreply.github.com>
2019-07-13 04:09:46 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,34 +1,31 @@
 var header = $("header"),
-    headerHeight = header.height(), // Get height of header
- 	 	logo = $(".logo"), // Get the logo
- 	 	logoHeight = logo.height(), // Get logo height
- 	 	scrollTo = 120; // Animation until scrolled to this point
+    headerHeight = header.height(), 
+ 	 	logo = $(".logo"), 
+ 	 	logoHeight = logo.height(), 
+ 	 	scrollTo = 120; 
 
-// Scroll function
 $(window).on("scroll", function() {
-  // Dynamic variables - That do change while scrolling
-  var yPos = $(this).scrollTop(), // Get the scroll Y-position
-  	  yPer = (yPos / scrollTo); // Calculate percenage of scroll
+  var yPos = $(this).scrollTop(), 
+  	  yPer = (yPos / scrollTo); 
 
-  // If percentage is over 100, set to 100
   if (yPer > 1) {
     yPer = 1;
   }
-  // Dynamic variables for the elements
-  var logoPos = ( -1*(yPer*50)+50), // Calculate position of logo (starting from 50%)
-    	logoSize = ((headerHeight*yPer)-(logoHeight*yPer)+logoHeight), // Calculate new size height for logo
-    	headerPos = ((yPer * headerHeight) - headerHeight); // Calculate position of header (starting from minus the height of itself)
 
-  // Change the top, left, transform and height of the logo
+  var logoPos = ( -1*(yPer*50)+50), 
+    	logoSize = ((headerHeight*yPer)-(logoHeight*yPer)+logoHeight), 
+    	headerPos = ((yPer * headerHeight) - headerHeight); 
+	
+
   logo.css({
     top: logoPos + "%",
     left: logoPos + "%",
     transform: "translate3d(-" + logoPos + "%,-" + logoPos + "%,0)",
     height: logoSize
   });
-  // Change the transform and opacity of the header
+
   $('header').css({
-    //transform: "translate3d(0," + headerPos + "px,0)",
+
 		top: headerPos,
     opacity: yPer
   });
